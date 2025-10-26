0759107effaae20fe2c99c260863c0d29e29de2d
fersatgit <122711455+fersatgit@users.noreply.github.com>
2025-10-26 10:41:09 +0500

    Add files via upload

--- x64/build.bat
+++ x64/build.bat
@@ -1,9 +1,9 @@
 set fasmpath=D:\Program Files\FASM
 set include=%fasmpath%\include
 
-"%fasmpath%\fasm.exe" WEBP4CRx64.asm WEBP4CRx64.obj
+"%fasmpath%\fasm.exe" WEBP4CDRx64.asm WEBP4CDRx64.obj
 
-..\link\link.exe /DEBUG:NONE /NOIMPLIB /NOEXP /ENTRY:start /MACHINE:X64 /DLL /EXPORT:AttachPlugin /OPT:REF /MERGE:data=.text /MERGE:.flat=.text /MERGE:.data=.text /MERGE:.rdata=.text /MERGE:.pdata=.text /SECTION:.text,ERW /FILEALIGN:512  /RELEASE /SUBSYSTEM:WINDOWS /libpath:"lib" kernel32.lib user32.lib shell32.lib ole32.lib oleaut32.lib ucrt.lib vcruntime.lib libwebp.lib WEBP4CRx64.obj /OUT:"WEBP4CRx64.cpg"
+..\link\link.exe /DEBUG:NONE /NOIMPLIB /NOEXP /ENTRY:start /MACHINE:X64 /DLL /EXPORT:AttachPlugin /OPT:REF /MERGE:data=.text /MERGE:.flat=.text /MERGE:.data=.text /MERGE:.rdata=.text /MERGE:.pdata=.text /SECTION:.text,ERW /FILEALIGN:512  /RELEASE /SUBSYSTEM:WINDOWS /libpath:"lib" kernel32.lib user32.lib shell32.lib ole32.lib oleaut32.lib ucrt.lib vcruntime.lib libwebp.lib WEBP4CDRx64.obj /OUT:"WEBP4CDRx64.cpg"
 
-del WEBP4CRx64.obj
+del WEBP4CDRx64.obj
 pause
